a3d0685c901b479e28255447637b6070c8b4afb9
tastytea <tastytea@tastytea.de>
2016-11-14 03:02:14 +0100

    whitespace fix

--- src/configfile.hpp
+++ src/configfile.hpp
@@ -1,21 +1,21 @@
 // Very limited, but easy to use, wrapper for libconfig++
 
 /*
- *	Copyright © 2016 tastytea <tastytea@tastytea.de>
+ *  Copyright © 2016 tastytea <tastytea@tastytea.de>
  *
- *	This file is part of seriespl.
+ *  This file is part of seriespl.
  *
- *	seriespl is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, version 2 of the License.
+ *  seriespl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2 of the License.
  *
- *	seriespl is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *  seriespl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License along
- *	with seriespl. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ *  You should have received a copy of the GNU General Public License along
+ *  with seriespl. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
  *
  ******************************************************************************/
 
--- src/playlist.hpp
+++ src/playlist.hpp
@@ -1,20 +1,20 @@
 // Playlist printing
 /*
- *	Copyright © 2016 tastytea <tastytea@tastytea.de>
+ *  Copyright © 2016 tastytea <tastytea@tastytea.de>
  *
- *	This file is part of seriespl.
+ *  This file is part of seriespl.
  *
- *	seriespl is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, version 2 of the License.
+ *  seriespl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2 of the License.
  *
- *	seriespl is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *  seriespl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License along
- *	with seriespl. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ *  You should have received a copy of the GNU General Public License along
+ *  with seriespl. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
  *
  ******************************************************************************/
 
